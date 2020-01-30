4cf7d9964ca18d6ca7e4346ac9a02effe5e47b15
tukuanchung <tukuanchung@gmail.com>
2020-01-30 23:11:12 +0800

    url routing
    Signed-off-by: tukuanchung <tukuanchung@gmail.com>

--- spring-boot-demo/src/main/java/com/kctud/web/HelloController.java
+++ spring-boot-demo/src/main/java/com/kctud/web/HelloController.java
@@ -1,16 +1,32 @@
 package com.kctud.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *  Created by KCTUD on 2019/01/30
  */
-@RestController
+//@RestController
+@Controller
+@RequestMapping("/api")
 public class HelloController {
 
-    @RequestMapping("/")
+//    @RequestMapping(value="/say", method=RequestMethod.GET)
+//    @RequestMapping("/say")
+    @PostMapping("/say")
     public String hello(){
         return "Hello Spring boot";
+    }
+
+    @GetMapping("/books")
+    @ResponseBody
+    public Object getAll(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("name","hello");
+        map.put("age","18");
+        return map;
     }
 }
