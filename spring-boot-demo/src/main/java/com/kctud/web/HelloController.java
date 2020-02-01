8b88e65fd655b843dc592b61ecc2532c1afe8f7e
tukuanchung <tukuanchung@gmail.com>
2020-02-01 18:29:01 +0800

    @PathVariable
    Signed-off-by: tukuanchung <tukuanchung@gmail.com>

--- spring-boot-demo/src/main/java/com/kctud/web/HelloController.java
+++ spring-boot-demo/src/main/java/com/kctud/web/HelloController.java
@@ -9,9 +9,9 @@ import java.util.Map;
 /**
  *  Created by KCTUD on 2019/01/30
  */
-//@RestController
-@Controller
-@RequestMapping("/api")
+@RestController
+//@Controller
+@RequestMapping("/api/v1")
 public class HelloController {
 
 //    @RequestMapping(value="/say", method=RequestMethod.GET)
@@ -22,11 +22,28 @@ public class HelloController {
     }
 
     @GetMapping("/books")
-    @ResponseBody
+//    @ResponseBody
     public Object getAll(){
         Map<String, Object> map = new HashMap<>();
         map.put("name","hello");
         map.put("age","18");
         return map;
+    }
+
+    /**
+     * regexPattern: {paramName:regexPattern}
+     * @param id
+     * @param username
+     * @return
+     */
+    @GetMapping("/books/{id}/{username:[a-z_]+}")
+    public Object getOne(@PathVariable long id, @PathVariable String username){
+        System.out.print(" --- id: " + id + "  username:" + username);
+        Map<String, Object> book = new HashMap<>();
+        book.put("name","愛在飛行");
+        book.put("isbn","10100000200");
+        book.put("author","杜貫仲");
+        book.put("username",username);
+        return book;
     }
 }
