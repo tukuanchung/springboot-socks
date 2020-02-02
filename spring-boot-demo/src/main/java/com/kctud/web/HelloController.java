ef27594391f8f9584f03cd009428a3a98aebea68
tukuanchung <tukuanchung@gmail.com>
2020-02-02 23:11:10 +0800

    how to set application.yml
    Signed-off-by: tukuanchung <tukuanchung@gmail.com>

--- spring-boot-demo/src/main/java/com/kctud/web/HelloController.java
+++ spring-boot-demo/src/main/java/com/kctud/web/HelloController.java
@@ -1,5 +1,8 @@
 package com.kctud.web;
 
+import com.kctud.domain.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,9 @@ import java.util.Map;
 //@Controller
 @RequestMapping("/api/v1")
 public class HelloController {
+
+    @Autowired
+    private Book book;
 
 //    @RequestMapping(value="/say", method=RequestMethod.GET)
 //    @RequestMapping("/say")
@@ -50,17 +56,18 @@ public class HelloController {
     /**
      * regexPattern: {paramName:regexPattern}
      * @param id
-     * @param username
+     * @param
      * @return
      */
-    @GetMapping("{id}/books/{username:[a-z_]+}")
-    public Object getOne(@PathVariable long id, @PathVariable String username){
-        System.out.print(" --- id: " + id + "  username:" + username);
-        Map<String, Object> book = new HashMap<>();
-        book.put("name","愛在飛行");
-        book.put("isbn","10100000200");
-        book.put("author","杜貫仲");
-        book.put("username",username);
+    @GetMapping("/books/{id}")
+    public Object getOne(@PathVariable long id){
+//        System.out.print(" --- id: " + id + "  username:" + username);
+//        Map<String, Object> book = new HashMap<>();
+//        book.put("name",name);
+//        book.put("isbn",isbn);
+//        book.put("author",author);
+//        book.put("description", description);
+//        book.put("username",username);
         return book;
     }
 
