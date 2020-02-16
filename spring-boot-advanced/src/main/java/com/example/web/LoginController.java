276d104ec14e830a764830fb4fb3f3d45f2e85c8
tukuanchung <tukuanchung@gmail.com>
2020-02-16 13:32:34 +0800

    a common interface
    Signed-off-by: tukuanchung <tukuanchung@gmail.com>

--- spring-boot-advanced/src/main/java/com/example/web/LoginController.java
+++ spring-boot-advanced/src/main/java/com/example/web/LoginController.java
@@ -2,6 +2,9 @@ package com.example.web;
 
 import com.example.dmain.User;
 import com.example.dmain.UserRepository;
+import com.example.form.UserForm;
+import com.example.form.UserFormConvert;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +21,7 @@ public class LoginController {
     private UserRepository userRepository;
 
     @GetMapping("/register")
-    public String register(){
+    public String registerPage(){
         return "register";
     }
     @GetMapping("/login")
@@ -27,16 +30,10 @@ public class LoginController {
     }
 
     @PostMapping("/register")
-    public String registerPost(@RequestParam String username,
-                               @RequestParam String password,
-                               @RequestParam String email,
-                               @RequestParam Integer phone){
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setEmail(email);
-        user.setPhone(phone);
-        userRepository.save(user);
-        return "redirect:/login";
+    public String register(UserForm userForm){
+       User user = new UserFormConvert().convert(userForm);
+       userRepository.save(user);
+       return "redirect:/login";
     }
+
 }
